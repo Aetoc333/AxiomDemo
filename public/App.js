2001b74b45c9663bab83015d0a4f76055bfad809
Aetoc333 <matt.mimnaugh@gmail.com>
2020-03-26 23:53:40 -0600

    ballz

---
+++ public/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import { BrowserRouter as Router, Route } from 'react-router-dom'
+// import Navigation from './components/navigation'
+import PhaseSlider from "./PhaseSlider";
+
+const App = () => (
+  <div className="app">
+    <PhaseSlider />
+  </div>
+);
+
+export default App;
